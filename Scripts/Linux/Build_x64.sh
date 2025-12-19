cc7d56d651858f495483ae91fd296105ed001757
Stuart Millman <stu.millman15@gmail.com>
2025-12-19 21:11:30 +0000

    Fixup win/linux scripts to have default version + separate x64 version

--- Scripts/Linux/Build_x64.sh
+++ Scripts/Linux/Build_x64.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 
-premake5 gmake2 --file=premake5-native.lua
+premake5 gmake2 --file=premake5-native.lua --arch=x86_64
 premake5 vs2022 --file=premake5-managed.lua
 make -j$(($(nproc) - 1))
 dotnet build CoralManaged.sln
