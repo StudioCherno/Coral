89d044bcfb423e32ba8b2c22d283b05b45d3112c
Stuart Millman <stu.millman15@gmail.com>
2025-12-19 14:59:33 +0000

    Fix ensure premake5 generates x64 solution and C++ project on Windows/Linux

--- Scripts/Windows/Setup.bat
+++ Scripts/Windows/Setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\..\
-premake5 vs2022 --file=premake5-native.lua
+premake5 vs2022 --file=premake5-native.lua --arch=x86_64
 premake5 vs2022 --file=premake5-managed.lua
 cd .\Scripts\Windows\
